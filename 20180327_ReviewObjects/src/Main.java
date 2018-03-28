b4fbf5cd2b7d0555d64daffe779f4be15896ee8b
J. Evan Noynaert <noynaert@missouriwestern.edu>
2018-03-27 20:16:17 -0500

    Arrays of objects

--- 20180327_ReviewObjects/src/Main.java
+++ 20180327_ReviewObjects/src/Main.java
@@ -11,26 +11,30 @@ public class Main {
 	    scifi.setAuthor("Sanderson, Brandon");
 	    scifi.setTitle("The Way of Kings");
 	    scifi.setPages(1023);
-
-	    System.out.println(scifi.getTitle() +" by " + scifi.getAuthor() );
-        System.out.println(scifi.getTitle() +" by " + scifi.getProperName() );
-        System.out.println(french.getTitle() +" by " + french.getProperName() );
-//////////////////////////////////////
         Book history = new Book("Rise and Fall of the Roman Empire", "",800);
-        System.out.println(history.getTitle() +" by " + history.getProperName() );
-
-        System.out.println(scifi);
-        System.out.println(french);
-        System.out.println(history);
-
-        System.out.println(scifi.getBiblio());
-        Book b = readBookFromKeyboard();
-        System.out.println(b);
-
-        //
 
 
+        int n = 5;
+        Book[] library = new Book[n];
+        library[0] = scifi;
+        library[1] = french;
+        library[2] = history;
+        library[3] = new Book("Programming in Java","Liang", 1595);
+        library[4] = new Book("Color of War", "Sanderson, Brandon", 475);
+
+        printBooks(library,n);
     }
+
+    private static void printBooks(Book[] library, int n) {
+        System.out.println("---------------------");
+        int pageTotal = 0;
+        for(int i=0;i<n;i++){
+            pageTotal += library[i].getPages();
+            System.out.println(library[i]);
+        }
+        System.out.printf("Average size: %1.0f pages",(double)pageTotal/n);
+    }
+
     public static Book readBookFromKeyboard(){
         Book result = null;
         String title;
