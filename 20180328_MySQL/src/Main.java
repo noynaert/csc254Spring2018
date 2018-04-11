afe869cbbfab8d385e5b6af912169ead3e8a2f2c
J. Evan Noynaert <noynaert@missouriwestern.edu>
2018-04-10 19:44:30 -0500

    Reading from database and building both an array and an arrayList

--- 20180328_MySQL/src/Main.java
+++ 20180328_MySQL/src/Main.java
@@ -1,13 +1,119 @@
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     // https://mwsu.zoom.us/my/noynaert
     public static void main(String[] args) {
 	    Stock stk = new Stock("PIH","1347 Property Insurance Holdings, Inc.", 7);
 	    System.out.println(stk);
-	    printAll("GO");
+	    //printAll("GO");
+        int max = 20;
+        Stock[] list = new Stock[max];
+        int n = buildTable("GO", list);
+        Arrays.sort(list,0, n);
+        printList(list,n);
+        ArrayList<Stock> ar = new ArrayList<>();
+        buildTable("E", ar);
+        printList(ar);
+    }
+    public  static<T> void printList(T[] list, int n) {
+          for(int i=0;i<n;i++) {
+              System.out.printf("[%d] %s\n", i, list[i]);
+          }
+    }
+    public static<T> void printList(ArrayList<T>list){
+        for(T stk: list){
+            System.out.println(stk);
+        }
+    }
+    public static void buildTable(String symbol, ArrayList<Stock> list){
+        int n = 0;
+        String host = "jdbc:mysql://turing.cs.missouriwestern.edu:3306/misc";
+        String user = "csc254";
+        String password="age126";
+
+        Connection conn;
+        Statement stmt;
+        ResultSet rs;
+
+        String queryString = "SELECT symbol, name, lastSale FROM stocks WHERE symbol like ";
+        queryString += String.format("'%%%s%%'", symbol);
+        System.out.println(queryString);
+
+        try {
+            conn = DriverManager.getConnection(host, user, password);
+            if(conn!=null){
+                System.out.println("Successful connection");
+            }else{
+                System.out.println("Connection failed");
+                System.exit(2);
+            }
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(queryString);
+
+            while(rs.next()){
+                String abbreviation = rs.getString("symbol");
+                String name = rs.getString("name");
+                int lastSale = rs.getInt("lastSale");
+                Stock stk = new Stock(abbreviation, name, lastSale);
+                list.add(stk);
+            }
+
+
+            conn.close();
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+        }
 
     }
+
+    public static int buildTable(String symbol, Stock[] list){
+        int n = 0;
+        String host = "jdbc:mysql://turing.cs.missouriwestern.edu:3306/misc";
+        String user = "csc254";
+        String password="age126";
+
+        Connection conn;
+        Statement stmt;
+        ResultSet rs;
+
+        String queryString = "SELECT symbol, name, lastSale FROM stocks WHERE symbol like ";
+        queryString += String.format("'%%%s%%'", symbol);
+        System.out.println(queryString);
+
+        try {
+            conn = DriverManager.getConnection(host, user, password);
+            if(conn!=null){
+                System.out.println("Successful connection");
+            }else{
+                System.out.println("Connection failed");
+                System.exit(2);
+            }
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(queryString);
+
+            while(rs.next()){
+                String abbreviation = rs.getString("symbol");
+                String name = rs.getString("name");
+                int lastSale = rs.getInt("lastSale");
+                Stock stk = new Stock(abbreviation, name, lastSale);
+                list[n++] = stk;
+                //n++;
+            }
+
+
+            conn.close();
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+        }
+        return n;
+    }
+
     public static void printAll(String symbol){
         String host = "jdbc:mysql://turing.cs.missouriwestern.edu:3306/misc";
         String user = "csc254";
@@ -18,7 +124,7 @@ public class Main {
         ResultSet rs;
 
         String queryString = "SELECT symbol, name, lastSale FROM stocks WHERE symbol like ";
-        queryString += String.format("'%s%%'", symbol);
+        queryString += String.format("'%%%s%%'", symbol);
         System.out.println(queryString);
 
         try {
--- 20180328_MySQL/src/Stock.java
+++ 20180328_MySQL/src/Stock.java
@@ -1,18 +1,65 @@
-public class Stock {
+public class Stock implements Comparable<Stock>{
     private String symbol;
     private String name;
     private int lastSale;
 
-    public Stock(String symbol, String name, int lastSale){
+    public Stock(String symbol, String name, int lastSale) {
+       setSymbol(symbol);
+       setName(name);
+       setLastSale(lastSale);
+    }
+    /*  Will not work because the method signature is the same as above
+    public Stock(String name, String symbol, int lastSale){
+    }
+    */
+    public Stock(int lastSale, String symbol, String name){
+        this(symbol, name, lastSale);
+    }
+    public Stock(String symbol, String name){
+        this(symbol, name, 0);
+    }
+    public Stock(String symbol){
+        this(symbol, "UNKNOWN", 0);
+    }
+    public Stock(){
+        this("???");
+    }
+
+    public void setSymbol(String symbol) {
         this.symbol = symbol;
+    }
+
+    public void setName(String name) {
         this.name = name;
-        this.lastSale = lastSale;
+    }
+
+    public void setLastSale(int lastSale) {
+        this.lastSale = (lastSale < 0) ? 0 : lastSale;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLastSale() {
+        return lastSale;
     }
 
     @Override
     public String toString() {
-        return  "symbol='" + symbol + '\'' +
-                ", name='" + name + '\'' +
-                ", lastSale=" + lastSale;
+        return String.format("%s (%s) Closed at %d", name, getSymbol(), lastSale);
+    }
+
+    public int compareTo(Stock other){
+        int result = 0;
+        result = name.compareTo(other.name);
+        if(result == 0){
+            result = this.lastSale - other.lastSale;
+        }
+        return result;
     }
 }
